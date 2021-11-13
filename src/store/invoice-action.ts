1ba86c95346ea6b18537d6becf503f11493ed406
mac <mac.wikrom@gmail.com>
2021-11-13 23:24:22 +0700

    use local storage

--- src/store/invoice-action.ts
+++ src/store/invoice-action.ts
@@ -6,8 +6,15 @@ import axios from "axios";
 export const fetchInvoicesData = () => {
   return async (dispatch: AppDispatch) => {
     try {
-      const data = await axios.get(process.env.PUBLIC_URL + "/assets/data.json");
-      dispatch(invoiceActions.replaceInvoices({ invoices: data.data }));
+      const localData = localStorage.getItem("data");
+      if (localData) {
+        const data = JSON.parse(localData);
+        dispatch(invoiceActions.replaceInvoices({ invoices: data }));
+      } else {
+        const response = await axios.get(process.env.PUBLIC_URL + "/assets/data.json");
+        localStorage.setItem("data", JSON.stringify(response.data));
+        dispatch(invoiceActions.replaceInvoices({ invoices: response.data }));
+      }
     } catch (error) {
       console.log(error);
     }
@@ -17,8 +24,15 @@ export const fetchInvoicesData = () => {
 export const fetchInvoiceItemData = (id: string) => {
   return async (dispatch: AppDispatch) => {
     try {
-      const result = await axios.get<invoiceType[]>(process.env.PUBLIC_URL + "/assets/data.json");
-      const dataResult = result.data.find((item) => item.id === id);
+      let data: invoiceType[] | undefined;
+      const localData = localStorage.getItem("data");
+      if (localData) {
+        data = JSON.parse(localData);
+      } else {
+        const response = await axios.get(process.env.PUBLIC_URL + "/assets/data.json");
+        data = response.data;
+      }
+      const dataResult = data ? data.find((item) => item.id === id) : null;
       dispatch(invoiceActions.replaceInvoiceItem({ invoiceItem: dataResult }));
     } catch (error) {
       console.log(error);
